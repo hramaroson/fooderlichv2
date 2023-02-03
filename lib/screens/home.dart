68058f864404bf84381b351c3f78b8bb6d2e0865
Herizo Ramaroson <39248388+hramaroson@users.noreply.github.com>
2023-02-04 01:41:24 +0300

    Update profile button onTap() event

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -83,7 +83,11 @@ class HomeState extends State<Home> {
           ),
         ),
         onTap: () {
-          // TODO: Navigate to profile screen
+           context.goNamed(
+            'profile',
+            params: {
+              'tab' : '$currentTab'
+            });
         },
       ),
     );
